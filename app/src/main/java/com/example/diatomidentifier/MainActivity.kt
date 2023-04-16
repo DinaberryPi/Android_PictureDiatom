2ab54beea6f63104cfca032baf50b1a766ddfc5f
DinaberryPi <dinahenrykyy@gmail.com>
2023-04-16 18:58:19 -0500

    Camera set up

--- app/src/main/java/com/example/diatomidentifier/MainActivity.kt
+++ app/src/main/java/com/example/diatomidentifier/MainActivity.kt
@@ -1,13 +1,51 @@
+/*
+ * Created by ishaanjav
+ * github.com/ishaanjav
+*/
 package com.example.diatomidentifier
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var result: TextView? = null
+    private var confidence: TextView? = null
+    private var imageView: ImageView? = null
+    private var picture: Button? = null
+    private var imageSize = 224
+
+    private val cameraActivityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                // Handle the captured image here
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setSupportActionBar(findViewById(R.id.toolbar))
+        result = findViewById<TextView>(R.id.result)
+        confidence = findViewById<TextView>(R.id.confidence)
+        imageView = findViewById<ImageView>(R.id.imageView)
+        picture = findViewById<Button>(R.id.button)
+        picture?.setOnClickListener(View.OnClickListener {
+            // Launch camera if we have permission
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                cameraActivityResultLauncher.launch(cameraIntent)
+            } else {
+                //Request camera permission if we don't have it.
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
+            }
+        })
     }
 }
