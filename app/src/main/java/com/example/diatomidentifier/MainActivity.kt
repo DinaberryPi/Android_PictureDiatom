221099df1874fca442fe4a1b2fe4a0bef3e5234a
DinaberryPi <dinahenrykyy@gmail.com>
2023-04-17 01:38:24 -0500

    Set up Result

--- app/src/main/java/com/example/diatomidentifier/MainActivity.kt
+++ app/src/main/java/com/example/diatomidentifier/MainActivity.kt
@@ -3,49 +3,121 @@
  * github.com/ishaanjav
 */
 package com.example.diatomidentifier
-
+import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.media.ThumbnailUtils
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
+import com.example.diatomidentifier.ml.Model
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
-    private var result: TextView? = null
-    private var confidence: TextView? = null
-    private var imageView: ImageView? = null
-    private var picture: Button? = null
-    private var imageSize = 224
 
-    private val cameraActivityResultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                // Handle the captured image here
-            }
-        }
+    private lateinit var result: TextView
+    private lateinit var confidence: TextView
+    private lateinit var imageView: ImageView
+    private lateinit var picture: Button
+    private var imageSize = 224
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        result = findViewById<TextView>(R.id.result)
-        confidence = findViewById<TextView>(R.id.confidence)
-        imageView = findViewById<ImageView>(R.id.imageView)
-        picture = findViewById<Button>(R.id.button)
-        picture?.setOnClickListener(View.OnClickListener {
+
+        result = findViewById(R.id.result)
+        confidence = findViewById(R.id.confidence)
+        imageView = findViewById(R.id.imageView)
+        picture = findViewById(R.id.button)
+
+        picture.setOnClickListener(View.OnClickListener {
             // Launch camera if we have permission
             if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                 val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                cameraActivityResultLauncher.launch(cameraIntent)
+                startActivityForResult(cameraIntent, 1)
             } else {
                 //Request camera permission if we don't have it.
                 requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
             }
         })
     }
+    fun classifyImage(image: Bitmap) {
+        try {
+            val model = Model.newInstance(applicationContext)
+
+            // Creates inputs for reference.
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+            val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+            byteBuffer.order(ByteOrder.nativeOrder())
+
+            // get 1D array of 224 * 224 pixels in image
+            val intValues = IntArray(imageSize * imageSize)
+            image.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
+
+            // iterate over pixels and extract R, G, and B values. Add to bytebuffer.
+            var pixel = 0
+            for (i in 0 until imageSize) {
+                for (j in 0 until imageSize) {
+                    val `val` = intValues[pixel++] // RGB
+                    byteBuffer.putFloat(((`val` shr 16) and 0xFF) * (1f / 255f))
+                    byteBuffer.putFloat(((`val` shr 8) and 0xFF) * (1f / 255f))
+                    byteBuffer.putFloat((`val` and 0xFF) * (1f / 255f))
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer)
+
+            // Runs model inference and gets result.
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.getOutputFeature0AsTensorBuffer()
+
+            val confidences = outputFeature0.floatArray
+            // find the index of the class with the biggest confidence.
+            var maxPos = 0
+            var maxConfidence = 0f
+            for (i in confidences.indices) {
+                if (confidences[i] > maxConfidence) {
+                    maxConfidence = confidences[i]
+                    maxPos = i
+                }
+            }
+            val classes = arrayOf("Centric中心类", "Araphid无壳缝类", "Eunotioid短壳缝类", "Surirelloid双菱藻类")
+            result.setText(classes[maxPos])
+
+            var s = ""
+            for (i in classes.indices) {
+                s += String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100)
+            }
+            confidence.setText(s)
+
+            // Releases model resources if no longer used.
+            model.close()
+        } catch (e: IOException) {
+            // TODO Handle the exception
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            var image = data?.extras?.get("data") as? Bitmap
+            var dimension = min(image?.width ?: 0, image?.height ?: 0)
+            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension)
+            imageView.setImageBitmap(image)
+
+            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false)
+            classifyImage(image)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
